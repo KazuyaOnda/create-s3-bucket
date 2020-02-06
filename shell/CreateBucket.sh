e072f9dc1a1cbf913485f6817d621c3d0f850e9c
KazuyaOnda <nct_kazuya_onda@yahoo.co.jp>
2020-02-06 12:54:53 +0900

    develop

--- shell/CreateBucket.sh
+++ shell/CreateBucket.sh
@@ -11,7 +11,6 @@ CFTemplate="../yaml/CreateBucket.yml"
 echo '_/_/_/ Start Create CloudFormation Stack _/_/_/'
 aws cloudformation create-stack --stack-name $EnvCode-$StoreCode-create --template-body file://$CFTemplate --parameters ParameterKey=EnvCode,ParameterValue=$EnvCode ParameterKey=StoreCode,ParameterValue=$StoreCode --tags Key=ResourceGroup,Value=$ResourceGroup --profile PersonalAdminRole
 echo '_/_/_/ End Create CloudFormation Stack _/_/_/'
-echo ''
 
 ## Wait 10 Seconds S3 Bucket Create
 sleep 10s
@@ -21,19 +20,16 @@ sleep 10s
 echo '_/_/_/ Start ErrorCheck Lambda Add Permission _/_/_/'
 aws lambda add-permission --function-name test-check-s3-upload-object --statement-id $EnvCode-$StoreCode-upload-event --action lambda:InvokeFunction --principal s3.amazonaws.com --source-arn arn:aws:s3:::$EnvCode.$StoreCode --profile PersonalAdminRole
 echo '_/_/_/ End ErrorCheck Lambda Add Permission _/_/_/'
-echo ''
 
 # DiffCheck Lambda
 echo '_/_/_/ Start DiffCheck Lambda Add Permission _/_/_/'
 aws lambda add-permission --function-name test-check-s3-temp-object --statement-id $EnvCode-$StoreCode-temp-event --action lambda:InvokeFunction --principal s3.amazonaws.com --source-arn arn:aws:s3:::$EnvCode.$StoreCode --profile PersonalAdminRole
 echo '_/_/_/ End DiffCheck Lambda Add Permission _/_/_/'
-echo ''
 
 # AllTableReCreate Lambda
 echo '_/_/_/ Start AllTableReCreate Lambda Add Permission _/_/_/'
 aws lambda add-permission --function-name test-check-s3-diff-object --statement-id $EnvCode-$StoreCode-diff-event --action lambda:InvokeFunction --principal s3.amazonaws.com --source-arn arn:aws:s3:::$EnvCode.$StoreCode --profile PersonalAdminRole
 echo '_/_/_/ End AllTableReCreate Lambda Add Permission _/_/_/'
-echo ''
 
 ## Create Null Key for S3 Bucket
 # upload
@@ -46,7 +42,6 @@ aws s3api put-object --bucket $EnvCode.$StoreCode --key old/upload/ --profile Pe
 aws s3api put-object --bucket $EnvCode.$StoreCode --key old/checked/ --profile PersonalAdminRole
 aws s3api put-object --bucket $EnvCode.$StoreCode --key old/brand/ --profile PersonalAdminRole
 echo '_/_/_/ End Create Null Key for S3 _/_/_/'
-echo ''
 
 ## S3 Event Type(Lambda Function)
 # Create Object to 'upload/'
