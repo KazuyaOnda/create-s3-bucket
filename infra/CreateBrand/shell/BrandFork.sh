f8c00b1f8389b8da42dc02e559c4470807a22b7b
KazuyaOnda <nct_kazuya_onda@yahoo.co.jp>
2020-02-26 15:37:07 +0900

    add Argment check

--- infra/CreateBrand/shell/BrandFork.sh
+++ infra/CreateBrand/shell/BrandFork.sh
@@ -8,31 +8,35 @@ ACCOUNT_ID=$5
 ForkFlg=$6
 Brand=$7
 
-## Check if arguments are entered
-#if [ $# -ne 6 ]; then
-#  echo "引数が不足しています。環境変数を確認してください。"
-#  exit 1
-#fi
-
-echo 'ForkFlg="'$ForkFlg'"'
-echo 'Brand="'$Brand'"'
-
-## Get S3 notification-configuration
+## Check Brand Fork And Argment
 echo '_/_/_/ Check Brand Fork (AllBrand or OneBrand) _/_/_/'
 if [ $ForkFlg = "all" ]; then
+  if [ $# -lt 6 ]; then
+    echo "## 引数が不足しています。環境変数を確認してください。 ##"
+    exit 1
+  else
     while read Brand
     do
-        echo 'All Brand Create'
-        /bin/bash ${CODEBUILD_SRC_DIR}/infra/CreateBrand/shell/CreateBrand.sh ${EnvCode} ${StoreCode} ${ResourceGroup} ${Database} ${ACCOUNT_ID} ${Brand}
-        sleep 15
+      echo 'All Brand Create'
+      /bin/bash ${CODEBUILD_SRC_DIR}/infra/CreateBrand/shell/CreateBrand.sh ${EnvCode} ${StoreCode} ${ResourceGroup} ${Database} ${ACCOUNT_ID} ${Brand}
+      sleep 15
     done < ${CODEBUILD_SRC_DIR}/infra/CreateBrand/list/brand.list
+  fi
 elif [ $ForkFlg = "one" ]; then
-    echo 'One Brand Create ('${Brand}')'
-    /bin/bash ${CODEBUILD_SRC_DIR}/infra/CreateBrand/shell/CreateBrand.sh ${EnvCode} ${StoreCode} ${ResourceGroup} ${Database} ${ACCOUNT_ID} ${Brand}
-else
-    echo '### ForkFlgは「all」か「one」を指定してください ###'
-    echo '## 「one」を指定した場合は、brandに作成対象のブランド名を1つだけ入力してください ##'
+  if [ $Brand = "" ]; then
+    echo "## ForkFlgで「one」を指定した場合は、Brandに作成対象のブランド名を1つだけ入力してください ##"
     exit 1
+  elif [ $# -lt 7 ]; then
+    echo "## 引数が不足しています。環境変数を確認してください。 ##"
+    exit 1
+  else
+    echo '_/_/_/ One Brand Create ('${Brand}') _/_/_/'
+    /bin/bash ${CODEBUILD_SRC_DIR}/infra/CreateBrand/shell/CreateBrand.sh ${EnvCode} ${StoreCode} ${ResourceGroup} ${Database} ${ACCOUNT_ID} ${Brand}
+  fi
+else
+  echo '### ForkFlgは「all」か「one」を指定してください ###'
+  echo '## 「one」を指定した場合は、Brandに作成対象のブランド名を1つだけ入力してください ##'
+  exit 1
 fi
 
 exit 0
