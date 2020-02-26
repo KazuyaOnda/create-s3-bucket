db5eeb5cf4814d7c3304c7e56fb958fbfd556e28
KazuyaOnda <nct_kazuya_onda@yahoo.co.jp>
2020-02-26 15:58:53 +0900

    Error Message Add

--- infra/CreateBrand/shell/BrandFork.sh
+++ infra/CreateBrand/shell/BrandFork.sh
@@ -12,6 +12,7 @@ Brand=$7
 echo '_/_/_/ Check Brand Fork (AllBrand or OneBrand) _/_/_/'
 if [ $ForkFlg = "all" ]; then
   if [ $# -lt 6 ]; then
+    echo "*-*-*-*-*-*-*-*-*- エラー！！ -*-*-*-*-*-*-*-*-*"
     echo "## 引数が不足しています。環境変数を確認してください。 ##"
     exit 1
   else
@@ -24,9 +25,12 @@ if [ $ForkFlg = "all" ]; then
   fi
 elif [ $ForkFlg = "one" ]; then
   if [ -z $Brand ]; then
+    echo "*-*-*-*-*-*-*-*-*- エラー！！ -*-*-*-*-*-*-*-*-*"
+    echo "## 環境変数の「Brand」に値が入力されていないエラーです ##"
     echo "## ForkFlgで「one」を指定した場合は、Brandに作成対象のブランド名を1つだけ入力してください ##"
     exit 1
   elif [ $# -lt 7 ]; then
+    echo "*-*-*-*-*-*-*-*-*- エラー！！ -*-*-*-*-*-*-*-*-*"
     echo "## 引数が不足しています。環境変数を確認してください。 ##"
     echo "## ForkFlgで「one」を指定した場合は、Brandに作成対象のブランド名を1つだけ入力してください ##"
     exit 1
@@ -35,6 +39,7 @@ elif [ $ForkFlg = "one" ]; then
     /bin/bash ${CODEBUILD_SRC_DIR}/infra/CreateBrand/shell/CreateBrand.sh ${EnvCode} ${StoreCode} ${ResourceGroup} ${Database} ${ACCOUNT_ID} ${Brand}
   fi
 else
+  echo "*-*-*-*-*-*-*-*-*- エラー！！ -*-*-*-*-*-*-*-*-*"
   echo '### ForkFlgは「all」か「one」を指定してください ###'
   echo '## 「one」を指定した場合は、Brandに作成対象のブランド名を1つだけ入力してください ##'
   echo '## ForkFlgを正しく指定している場合は、他の環境変数が問題なく入力されていることを確認してください ##'
